7d2df4fe42967cc146c816a0a36f7ff9231a742a
Александр Шульдешов <qsteamk@gmail.com>
2021-08-08 18:09:15 +0300

    добавил аккордеон (десктоп)

--- source/js/files/accordion.js
+++ source/js/files/accordion.js
@@ -2,20 +2,5 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 (function () {
-  const mediaQueryMobile = window.matchMedia(`(max-width: 767px)`);
-  let contactsAccordion = null;
-
-  const handleMobileWidth = (mql) => {
-    if (mql.matches && !contactsAccordion) {
-      contactsAccordion = new Accordion(`.accordion-container`);
-    }
-
-    if (!mql.matches && contactsAccordion) {
-      contactsAccordion.destroy();
-      contactsAccordion = null;
-    }
-  };
-
-  window.addEventListener(`DOMContentLoaded `, handleMobileWidth(mediaQueryMobile));
-  mediaQueryMobile.addEventListener(`change`, handleMobileWidth);
+  const contactsAccordion = new Accordion(`.accordion-container`);
 })();
